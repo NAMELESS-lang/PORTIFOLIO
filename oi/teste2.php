db48ea36096af535725c47e80b00db5e97bf2b59
Matheus Fiedler Colla <matheusfiedlercolla@gmail.com>
2025-01-21 16:05:44 -0300

    altertações

---
+++ oi/teste2.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'teste.php';
+
+class Arma{
+    
+}
+
+$arma = new \Classeigual\PORT\Arma('oi');
+echo $arma->tipo;
---
+++ teste.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Classeigual\PORT;
+
+class Arma{
+    public $tipo;
+
+    public function __construct($tipo){
+        return $this->tipo = $tipo;
+    }
+}
