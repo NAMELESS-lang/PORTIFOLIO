382d6388c708167104c98b69e9ceb2d1e7a76a94
Matheus Fiedler Colla <matheusfiedlercolla@gmail.com>
2026-03-16 11:15:12 -0300

    Adicionado sistema de pular ferramentas em responsividade mobile

--- js/sobre_mim.js
+++ js/sobre_mim.js
@@ -1,16 +1,30 @@
 var indice_ferramenta = 0;
 var botao_esquerda = document.getElementById("seta_esquerda");
 var botao_direito = document.getElementById("seta_direita");
-var dicionario_elemento_ferramentas = {};
-
+var lista_ferramenta = ['apache.jpg','debian.png','docker.png','nginx.png',
+    'proxmox.png','windows.png','zabbix.png'];
+var nome_ferramenta = ['apache','debian','docker','nginx','proxmox','windows','zabbix']
 function trocar_ferramenta(sentido){
+    let icone_ferramenta = document.getElementById("imagem_ferramenta_mobile")
+    let paragrafo_ferramenta = document.getElementById("paragrafo_ferramenta_mobile")
+    
     if (sentido == "esquerda"){
-        if(indice_ferramenta == 0){
-            botao_esquerda.disabled = true
+        if(indice_ferramenta >0){
+            indice_ferramenta-=1
+        }else{
+            return
         }
-
+    }else if(sentido == "direita"){
+        if(indice_ferramenta < (lista_ferramenta.length-1)){
+            indice_ferramenta+=1
+        }else{
+            return
+        }
     }
-
+    
+    let icone = "../images/" + lista_ferramenta[indice_ferramenta]
+    icone_ferramenta.src = icone
+    paragrafo_ferramenta.innerHTML = nome_ferramenta[indice_ferramenta]
 }
 
 
