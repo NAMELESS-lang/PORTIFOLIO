da0311ed3fa67a0eee29696f569138c9bd7a9bb7
matheus <matheusfiedlercolla@gmail.com>
2026-05-24 09:26:04 -0300

    Adicionado menu hamburguer

--- js/home.js
+++ js/home.js
@@ -1,7 +1,14 @@
 function mostrar_contatos(){
     let div_contatos = document.getElementById("contatos");
+    for(let classes of div_contatos.classList.values()){
+        if (classes == "d-block"){
+           remover_contatos()
+           return
+        }
+    }
     div_contatos.classList.remove("d-none");
     div_contatos.classList.add("d-block");
+    div_contatos.style.animation="surgir_cima_baixo 0.5s linear";
     return
 }
 
@@ -9,5 +16,6 @@ function remover_contatos(){
     let div_contatos = document.getElementById("contatos");
     div_contatos.classList.remove("d-block");
     div_contatos.classList.add("d-none");
+
 }
 document.getElementById("botao_contato").addEventListener("click",()=>mostrar_contatos());
