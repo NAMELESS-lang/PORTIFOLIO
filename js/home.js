aca55d326239aec6aa0ad2fcb8bae97193fb111c
Matheus Fiedler Colla <matheusfiedlercolla@gmail.com>
2026-05-28 11:05:32 -0300

    Corrigido responsividade do mobile e menu

--- js/home.js
+++ js/home.js
@@ -1,20 +1,20 @@
 function mostrar_contatos(){
     let div_contatos = document.getElementById("contatos");
     for(let classes of div_contatos.classList.values()){
-        if (classes == "d-block"){
+        if (classes == "d-flex"){
            remover_contatos()
            return
         }
     }
     div_contatos.classList.remove("d-none");
-    div_contatos.classList.add("d-block");
+    div_contatos.classList.add("d-flex");
     div_contatos.style.animation="surgir_cima_baixo 0.5s linear";
     return
 }
 
 function remover_contatos(){
     let div_contatos = document.getElementById("contatos");
-    div_contatos.classList.remove("d-block");
+    div_contatos.classList.remove("d-flex");
     div_contatos.classList.add("d-none");
 
 }
